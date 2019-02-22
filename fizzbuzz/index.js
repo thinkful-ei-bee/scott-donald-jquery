23d326b851553fa527844f070fabcc8169a6eafd
Scott Dowding <scottddowding1279@gmail.com>
2019-02-22 16:27:23 -0500

    working version of fizzbuzz

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -1,4 +1,5 @@
 'use strict';
+/* global $ */
 
 function fizzbuzz(num) {
   if(num % 15 === 0) {
@@ -15,25 +16,33 @@ function fizzbuzz(num) {
   }
 }
 
-function fizzBuzzHTML(num){
-  for(let i=1; i <= num; i++) {
-    let fizzBuzzOutput = fizzbuzz(num);
-    if(typeof fizzBuzzOutput === 'string') {
-      $('.js-results').html(`<div class="fizz-buzz-item ${fizzBuzzOutput}">
+function fizzBuzzHTML(fizzBuzzOutput){
+  if(typeof fizzBuzzOutput === 'string') {
+    return `<div class="fizz-buzz-item ${fizzBuzzOutput}">
       <span>${fizzBuzzOutput}</span>
-      </div>`)
-    } else {
-      $('.js-results').html(`<div class="fizz-buzz-item">
+      </div>`;
+  } else {
+    return `<div class="fizz-buzz-item">
       <span>${fizzBuzzOutput}</span>
-      </div>`)
-    }
+      </div>`;
   }
 }
 
 $('#number-chooser').submit(function(event) {
   event.preventDefault();
   let numClicks = $('#number-choice').val();
+  const result = [];
   // console.log($('#number-choice').val());
-  fizzBuzzHTML(numClicks);
-})
+  //fizzBuzzHTML(numClicks);
+  for(let i=1; i <= numClicks; i++) {
+    let fizzBuzzOutput = fizzbuzz(i);
+    result.push(fizzBuzzOutput);
+  }
+  let HTMLstring = result.map(fizzBuzzHTML).join('');
+  $('.js-results').html(HTMLstring);
+  
+  //console.log(result);
+  //const map1 = result.map(fizzBuzzHTML());
+  //console.log(map1);
+});
 
